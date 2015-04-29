931fe43770eebb4e3adbebfd9a3a780e175c6a79
Jelle De Loecker <jelle@kipdola.be>
2015-04-29 22:37:34 +0200

    Initial commit

---
+++ assets/scripts/debugbar/debugbar_panel.js
@@ -0,0 +1,17 @@
+/**
+ * The DebugbarPanel class
+ *
+ * @constructor
+ *
+ * @author   Jelle De Loecker   <jelle@kipdola.be>
+ * @since    1.0.0
+ * @version  1.0.0
+ */
+var DebugbarPanel = Function.inherits('Informer', function DebugbarPanel(name, title) {
+
+	// The name of the panel
+	this.name = name;
+
+	// The title to show
+	this.title = title;
+});
---
+++ bootstrap.js
@@ -0,0 +1,29 @@
+// Enable classWideEvents for these classes
+Model.setStaticProperty('classWideEvents', true);
+Controller.setStaticProperty('classWideEvents', true);
+alchemy.hawkejs.constructor.ViewRender.setStaticProperty('classWideEvents', true);
+
+// Add the debugbar script
+alchemy.hawkejs.on({type: 'viewrender', status: 'begin'}, function onBegin(viewRender) {
+
+	var conduit = viewRender.conduit;
+
+	// Add the debugbar javascript files
+	viewRender.script(['debugbar/debugbar_panel', 'debugbar/core']);
+
+	// Add the debugbar stylesheets
+	viewRender.style('debugbar/core');
+
+	var debugbarData = {
+		session: conduit.session(),
+		headers: conduit.headers,
+		route: conduit.route,
+		languages: conduit.languages,
+		body: conduit.body,
+		cookies: conduit.cookies,
+		files: conduit.files,
+		url: conduit.url
+	};
+
+	viewRender.internal('debugbarData', debugbarData);
+});
